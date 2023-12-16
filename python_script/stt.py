396b2d817c7f07f45809795ce660399edfa65b9b
Chrystal Lim Jing Yee <chrystal.limjy@gmail.com>
2023-12-16 21:32:00 +0800

    commit python speech to text stuff

---
+++ python_script/stt.py
@@ -0,0 +1,38 @@
+#speech to text
+from vosk import Model, KaldiRecognizer
+import pyaudio
+
+model = Model("vosk-model-small-en-us-0.15")
+
+r = KaldiRecognizer(model, 48000)
+
+
+# Find the device index of the microphone you want to use
+def find_microphone_index():
+    audio = pyaudio.PyAudio()
+    for index in range(audio.get_device_count()):
+        if audio.get_device_info_by_index(index).get('maxInputChannels') > 0:
+            print(f"Microphone found at index {index}: {audio.get_device_info_by_index(index).get('name')}")
+            return index
+    print("No microphone found")
+    return None
+
+# Get the microphone index
+mic_index = find_microphone_index()
+if mic_index is None:
+    print("No microphone found, exiting")
+    exit()
+
+
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=48000, input=True, input_device_index=mic_index, frames_per_buffer=8192)
+stream.start_stream()
+
+while True:
+    data = stream.read(4096)
+    # if len(data) == 0:
+    #     break
+    if r.AcceptWaveform(data):
+        text = r.Result()
+        
+        print(text[14:-3])
