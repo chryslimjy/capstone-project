379d555ed8b8365c0bd70cdddf496a175519f592
Chrystal Lim Jing Yee <chrystal.limjy@gmail.com>
2024-02-25 23:13:06 +0800

    integrated python code to nodejs

--- python_script/app.py
+++ python_script/app.py
@@ -1,4 +1,5 @@
-#speech to text
+# speech to text
+import os
 from vosk import Model, KaldiRecognizer
 import pyaudio
 from flask import Flask, render_template
@@ -11,7 +12,9 @@ socketio = SocketIO(app, cors_allowed_origins='*')  # Enable CORS for all origin
 def index():
     return render_template('index.html')
 
-model = Model("vosk-model-small-en-us-0.15")
+#model = Model("vosk-model-small-en-us-0.15")
+model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "vosk-model-small-en-us-0.15")
+model = Model(model_path)
 r = KaldiRecognizer(model, 48000)
 
 # Find the device index of the microphone you want to use
@@ -42,10 +45,24 @@ def recognize_speech():
             recognized_text = text[14:-3]
             print(recognized_text)
             socketio.emit('speech_recognition', {'text': recognized_text})
-            
-            #socketio.emit('audio_data_received', {})  # Emit an event when audio data is received
+
+# Update the socketio.on_event to handle events from the Node.js application
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+# Start the Flask SocketIO server
+# if __name__ == '__main__':
+#     socketio.run(app, host='0.0.0.0', port=5000)  # Change host to allow connections from external sources
 
 socketio.start_background_task(target=recognize_speech)
 
 if __name__ == '__main__':
-    socketio.run(app, port=5000)  # Choose any available port
+    try:
+        socketio.run(app, host='localhost', port=5000, allow_unsafe_werkzeug=True)
+    except Exception as e:
+        print("An error occurred:", e)
